ce947c6abac1604008c1bc63f3e1784647a8ebc1
atawfique12 <abirtawfique12@outlook.com>
2019-11-23 18:09:29 -0500

    Fixed API issue - Wrong API Key - Replaced w/ new one.

--- api_call_test/apicall.js
+++ api_call_test/apicall.js
@@ -1,26 +1,24 @@
-let baseURL = 'https://random-word-api.herokuapp.com/all?key='
-const key = '3L1E3XTX'
-let randWordArray = []
-let getRandWord = async function () {
-  // get a list of words
-  const response = await axios.get(`${baseURL}${key}`)
-
-  for (let i = 0; i < 200; i++) {
-    let returnRandomizedWord = async function (response) {
-      response = response.data;
-      let num = Math.floor(Math.random() * response.length)
-      let randomWord = response[num]
-      randWordArray.push(randomWord)
-    }
-    returnRandomizedWord(response)
-  }
-
-}
-getRandWord()
-console.log(randWordArray)
-
-
 window.onload = function () {
   const button = document.querySelector('#button')
+  let baseURL = 'https://random-word-api.herokuapp.com/all?key='
+  const key = 'RHRG9QV3'
+  let randWordArray = []
+  let getRandWord = async function () {
+    // get a list of words
+    const response = await axios.get(`${baseURL}${key}`)
+
+    for (let i = 0; i < 200; i++) {
+      let returnRandomizedWord = async function (response) {
+        response = response.data;
+        let num = Math.floor(Math.random() * response.length)
+        let randomWord = response[num]
+        randWordArray.push(randomWord)
+      }
+      returnRandomizedWord(response)
+    }
+
+  }
+  getRandWord()
+  console.log(randWordArray)
 }
 
