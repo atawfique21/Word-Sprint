aaef5f145dc7c1030d91236e02ee2b105c711244
atawfique12 <abirtawfique12@outlook.com>
2019-11-25 17:03:53 -0500

    Now swaps through words but weird issue in console.

--- api_call_test/apicall.js
+++ api_call_test/apicall.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = async function () {
   let baseURL = 'https://random-word-api.herokuapp.com/all?key='
   const key = '00NP3T24'
   let randWordArray = []
@@ -6,9 +6,10 @@ window.onload = function () {
 
   let getRandWord = async function () {
     const response = await axios.get(`${baseURL}${key}`)
+
     console.log(response)
     for (let i = 0; i < 200; i++) {
-      let returnRandomizedWord = async function (response) {
+      let returnRandomizedWord = function (response) {
         response = response.data;
         let num = Math.floor(Math.random() * response.length)
         let randomWord = response[num]
@@ -17,12 +18,11 @@ window.onload = function () {
       returnRandomizedWord(response)
     }
   }
-  getRandWord()
+  await getRandWord()
 
   function isCorrect(evt, letterIndex, currentWordSel) {
     i = letterIndex;
     currentLetter = currentWordSel[i].innerHTML;
-    console.log(currentLetter)
     if (evt.key !== currentLetter) {
       currentWordSel[i].className = "";
       currentWordSel[i].classList.add("incorrect")
@@ -33,36 +33,50 @@ window.onload = function () {
       return true;
     }
   }
-
-
-  // window.onload = function () {
-  let letterIndex = 0;
-  let currentWordSel = document.querySelectorAll(`#w`);
   let arrayIndex = 0;
-
-  document.body.addEventListener('keydown', function (evt) {
-    console.log(evt)
-    if (evt.key === 'CapsLock' || evt.key === 'Meta' || evt.key === 'Shift') {
-      return;
-    } else if (evt.key === 'Backspace') {
-      currentWordSel[letterIndex].className = "";
-    } else if (!evt.key !== 'BackSpace' && currentWordSel[letterIndex].classList.contains("incorrect")) {
-      return;
-    } else {
-      if (!isCorrect(evt, letterIndex, currentWordSel)) {
-      } else {
-        letterIndex = letterIndex + 1;
-      }
-    }
-  })
-  console.log('hi')
-  console.log(randWordArray[0])
-
+  let wordLength = 0;
   function newWord(arr) {
+    wordLength = 0;
     let wordDiv = document.querySelector('#word')
     wordDiv.innerHTML = ""
     let word = arr[arrayIndex].split("")
+    console.log(word)
+    for (i = 0; i < word.length; i++) {
+      let newElem = document.createElement("h4")
+      newElem.setAttribute('char', 'w')
+      newElem.innerHTML = word[i]
+      wordDiv.appendChild(newElem)
+      wordLength++
+    }
+    let letterIndex = 0;
+    let currentWordSel = document.querySelectorAll(`h4`);
+
+    document.body.addEventListener('keydown', function (evt) {
+      if (evt.key === 'CapsLock' || evt.key === 'Meta' || evt.key === 'Shift') {
+        return;
+      } else if (evt.key === 'Backspace') {
+        console.log(currentWordSel, letterIndex)
+        console.log(currentWordSel[letterIndex])
+        currentWordSel[letterIndex].className = "";
+      } else if (!evt.key !== 'BackSpace' && currentWordSel[letterIndex].classList.contains("incorrect")) {
+        return;
+      } else {
+        if (!isCorrect(evt, letterIndex, currentWordSel)) {
+        } else {
+          letterIndex = letterIndex + 1;
+          console.log(letterIndex)
+          if (letterIndex === wordLength) {
+            console.log('hi')
+            newWord(randWordArray)
+          }
+        }
+      }
+    })
+    arrayIndex++
   }
 
   newWord(randWordArray)
+
+  // window.onload = function () {
+
 }
