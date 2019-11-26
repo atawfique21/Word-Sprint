39a03413fac761196839e538aec218e47b7fde12
atawfique12 <abirtawfique12@outlook.com>
2019-11-26 14:31:44 -0500

    Currently working on stopping game when timer is zero

--- api_call_test/apicall.js
+++ api_call_test/apicall.js
@@ -1,67 +1,37 @@
-window.onload = async function () {
-  let baseURL = 'https://random-word-api.herokuapp.com/all?key='
-  const key = '00NP3T24'
-  let randWordArray = ["another", "then", "also", "done", "generally", "back", "public", "copper", "quickly", "heard", "cattle", "program", "layers", "hidden", "gentle", "shelf", "danger", "fire", "carried", "frog", "shine", "between", "straw", "else", "cat", "nearly", "studying", "those", "pour", "might", "excellent", "planning", "ring", "pride", "proper", "stepped", "highway", "driver", "charge", "making", "four", "sold", "fact", "hard", "travel", "happen", "present", "understanding", "anything", "newspaper",
-    "eventually", "cow", "ride", "written", "serious", "honor", "eventually", "hair", "unless", "dog", "cap", "declared", "stiff", "clear", "garden", "known", "process", "tongue", "joined", "shadow", "subject", "pull", "silly", "breath", "plural", "cream", "love", "thing", "afraid", "tent", "underline", "bus", "sheep", "hollow", "sad", "shout", "shore", "medicine", "image", "children", "making", "thread", "begun", "poet", "fifty", "record", "pink", "function", "disappear", "good"]
-  let score = 0;
+let randWordArray = ["another", "then", "also", "done", "generally", "back", "public", "copper", "quickly", "heard", "cattle", "program", "layers", "hidden", "gentle", "shelf", "danger", "fire", "carried", "frog", "shine", "between", "straw", "else", "cat", "nearly", "studying", "those", "pour", "might", "excellent", "planning", "ring", "pride", "proper", "stepped", "highway", "driver", "charge", "making", "four", "sold", "fact", "hard", "travel", "happen", "present", "understanding", "anything", "newspaper",
+  "eventually", "cow", "ride", "written", "serious", "honor", "eventually", "hair", "unless", "dog", "cap", "declared", "stiff", "clear", "garden", "known", "process", "tongue", "joined", "shadow", "subject", "pull", "silly", "breath", "plural", "cream", "love", "thing", "afraid", "tent", "underline", "bus", "sheep", "hollow", "sad", "shout", "shore", "medicine", "image", "children", "making", "thread", "begun", "poet", "fifty", "record", "pink", "function", "disappear", "good"]
+let score = 0;
 
-  function shuffle(arr) {
-    let shuffledDeck = arr;
-    let currentIndex = arr.length;
-    let temporaryValue;
-    let randomIndex;
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex); //create a number between 0 & 52
-      currentIndex-- //subtract one for the sake of not looping forever
-      temporaryValue = shuffledDeck[currentIndex] // deck[52]
-      shuffledDeck[currentIndex] = shuffledDeck[randomIndex] // deck[52] == deck[random]
-      shuffledDeck[randomIndex] = temporaryValue // deck[random] == tempvalue
-    }
-    return shuffledDeck;
-  }
-
-  shuffle(randWordArray)
-
-  // let getRandWord = async function () {
-  //   const response = await axios.get(`${baseURL}${key}`)
-  //   for (let i = 0; i < 200; i++) {
-  //     let returnRandomizedWord = function (response) {
-  //       response = response.data;
-  //       let num = Math.floor(Math.random() * response.length)
-  //       let randomWord = response[num]
-  //       randWordArray.push(randomWord)
-  //     }
-  //     returnRandomizedWord(response)
-  //   }
-  // }
-  // await getRandWord()
+function stopGame() {
+  document.body.removeEventListener('keydown', eventListener, false)
+}
 
 
+function startGame() {
+  document.querySelector('#start').style.display = "none";
   function startTimer() {
     let timeSelector = document.querySelector("#time > h3")
     let sec = 60;
     let timer = setInterval(function () {
       timeSelector.innerHTML = sec
       sec--;
-      if (sec < 0) {
+      if (sec < 55) {
         clearInterval(timer)
+        stopGame()
       }
     }, 1000);
   }
 
   startTimer()
 
+
+
+
   function addScore() {
     let scoreSelector = document.querySelector("#score > h3")
     newScore = score++
     scoreSelector.innerHTML = newScore;
   }
-
-  //once time hits 0, stopGame
-  //stop game should remove eventlistener from body which will freeze the game.
-  //start game again when newWord is called. Do through button click.
-
-
 
   function isCorrect(evt, letterIndex, currentWordSel) {
     i = letterIndex;
@@ -122,3 +92,41 @@ window.onload = async function () {
   }
   newWord(randWordArray)
 }
+
+window.onload = async function () {
+  let baseURL = 'https://random-word-api.herokuapp.com/all?key='
+  const key = '00NP3T24'
+
+  function shuffle(arr) {
+    let shuffledDeck = arr;
+    let currentIndex = arr.length;
+    let temporaryValue;
+    let randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex); //create a number between 0 & 52
+      currentIndex-- //subtract one for the sake of not looping forever
+      temporaryValue = shuffledDeck[currentIndex] // deck[52]
+      shuffledDeck[currentIndex] = shuffledDeck[randomIndex] // deck[52] == deck[random]
+      shuffledDeck[randomIndex] = temporaryValue // deck[random] == tempvalue
+    }
+    return shuffledDeck;
+  }
+  shuffle(randWordArray)
+
+  document.querySelector('#start').addEventListener('click', startGame)
+}
+
+// if using api, insert code below into window.onload and empty the randwordarray.
+  // let getRandWord = async function () {
+  //   const response = await axios.get(`${baseURL}${key}`)
+  //   for (let i = 0; i < 200; i++) {
+  //     let returnRandomizedWord = function (response) {
+  //       response = response.data;
+  //       let num = Math.floor(Math.random() * response.length)
+  //       let randomWord = response[num]
+  //       randWordArray.push(randomWord)
+  //     }
+  //     returnRandomizedWord(response)
+  //   }
+  // }
+  // await getRandWord()
